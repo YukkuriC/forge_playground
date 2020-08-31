f855e6071c249f530e880b6e4a79d838fb57e0ea
Yukkuri C <799433638@qq.com>
2020-08-31 17:18:36 +0800

    helper upgrade - register of register - more general interface

--- src/main/java/io/yukkuric/misc/Helper.java
+++ src/main/java/io/yukkuric/misc/Helper.java
@@ -6,11 +6,19 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 
 import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public final class Helper {
     public static ITextComponent GetLang(String lang, @Nullable TextFormatting clr, Object... args) {
         ITextComponent res = new TranslationTextComponent(lang, args);
         if (clr != null) res.setStyle(new Style().setColor(clr));
         return res;
+    }
+
+    public static String Camel2Snake(String camel) {
+        return Arrays.stream(camel.split("(?<=[a-z])(?=[A-Z])"))
+                .map(String::toLowerCase)
+                .collect(Collectors.joining("_"));
     }
 }
--- src/main/java/io/yukkuric/misc/RegHelper.java
+++ src/main/java/io/yukkuric/misc/RegHelper.java
@@ -7,10 +7,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 import net.minecraftforge.registries.RegistryManager;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class RegHelper {
     static IEventBus modBus;
@@ -23,23 +21,31 @@ public class RegHelper {
         defRegCache = new HashMap<>();
     }
 
-    public static <bC extends IForgeRegistryEntry<bC>> Function<Class<?>, RegistryObject<bC>>
-    Gen(Class<bC> baseCls, Class<?>[] constructors, Object... params) {
-        // pick DeferredRegister
+    // pick DeferredRegister
+    public static <bC extends IForgeRegistryEntry<bC>> DeferredRegister<bC> DefReg(Class<bC> baseCls) {
         DeferredRegister<bC> defReg;
         if (!defRegCache.containsKey(baseCls)) {
             defReg = new DeferredRegister<>(RegistryManager.ACTIVE.getRegistry(baseCls), modId);
             defRegCache.put(baseCls, defReg);
             defReg.register(modBus);
         } else defReg = (DeferredRegister<bC>) defRegCache.get(baseCls);
+        return defReg;
+    }
+
+    // (register from class) register
+    public static <bC extends IForgeRegistryEntry<bC>> Function<Object[], Function<Class<?>, RegistryObject<bC>>>
+    GenGen(Class<bC> baseCls, Class<?>[] constructors) {
+        return (Object... params) -> Gen(baseCls, constructors, params);
+    }
+
+    // register from class
+    public static <bC extends IForgeRegistryEntry<bC>> Function<Class<?>, RegistryObject<bC>>
+    Gen(Class<bC> baseCls, Class<?>[] constructors, Object... params) {
+        DeferredRegister<bC> defReg = DefReg(baseCls);
 
         // gen function
         return (Class<?> regCls) -> {
-            // snake_case from CamelCase
-            String idGen = Arrays.stream(regCls.getSimpleName().split("(?<=[a-z])(?=[A-Z])"))
-                    .map(String::toLowerCase)
-                    .collect(Collectors.joining("_"));
-
+            String idGen = Helper.Camel2Snake(regCls.getSimpleName());
             return defReg.register(idGen, () -> {
                 bC res = null;
                 try {
